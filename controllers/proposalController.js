483edd50c255d4060b0808738fe730f3762ab0ec
hossein-rahmati <rmthossein@gmail.com>
2024-08-24 21:55:06 +0330

    handle create proposal error

--- controllers/proposalController.js
+++ controllers/proposalController.js
@@ -1,6 +1,6 @@
 const Proposal = require('../models/proposalModel');
 
-exports.getAllProposals = async (req, res, next) => {
+exports.getAllProposals = async (req, res) => {
   const proposals = await Proposal.find();
 
   res.status(200).json({
@@ -10,12 +10,17 @@ exports.getAllProposals = async (req, res, next) => {
 };
 
 exports.createProposal = async (req, res, next) => {
-  console.log(req.body);
+  try {
+    const proposal = await Proposal.create(req.body);
 
-  const proposal = await Proposal.create(req.body);
-
-  res.status(201).json({
-    status: 'success',
-    data: proposal,
-  });
+    res.status(201).json({
+      status: 'success',
+      data: proposal,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+    next();
+  }
 };
